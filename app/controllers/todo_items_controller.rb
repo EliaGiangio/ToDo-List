236afb858285dbcf25e6d3251433377268eec94e
EliaGiangio <eliagiang@yahoo.com>
2022-02-19 17:57:00 +0100

    added completed at for items and button to delete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -8,12 +8,13 @@ class TodoItemsController < ApplicationController
 
   def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
+    
     if @todo_item.destroy
      flash[:success] = "Todo List item was deleted."
     else
      flash[:error] = "Todo List item could not be deleted."
     end
-    redirect_to @todo_list 
+    redirect_to @todo_list, status: :see_other
    end
 
   private
